079bbbe7d8079040410ca28be1968a761da85187
Ángel Cruz <59263971+AngelCruzL@users.noreply.github.com>
2023-05-25 13:04:33 -0600

    Add singleton typescript implementation

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
 // import './01-intro/01-array-methods.js'
 // import './01-intro/02-classes.ts'
-import './02-singleton/singleton.js'
+// import './02-singleton/singleton.js';
+import './02-singleton/singleton.ts';
